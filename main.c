7b5ab1f0e032adbf39ba21e918d72b7d9f00c867
mohamed thabet <92416406+MAthabet@users.noreply.github.com>
2022-09-08 06:25:13 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,20 +9,33 @@
 
 void main()
 {
+    timer_config_t timer;
+	timer.output_mode = PWM;
+	timer.clksrc =TIMER_PRESC_1024;
     
     dio_set_direction(LDR_PORT, LRD_PIN, DIO_DIRECTION_INPUT);
     dio_set_direction(LED_PORT, LED_PIN, DIO_DIRECTION_OUTPUT);
-    dio_set_direction(LM35_PORT, LM35_PIN, DIO_DIRECTION_INPUT);
+    
+    dio_set_direction(OPAMP10_PORT, OPAMP10_PIN, DIO_DIRECTION_INPUT);
+    dio_set_direction(OPAMP20_PORT, OPAMP30_PIN, DIO_DIRECTION_INPUT);
+    dio_set_direction(OPAMP30_PORT, OPAMP20_PIN, DIO_DIRECTION_INPUT);
+    
     dio_set_direction(FAN_PORT, FAN_PIN, DIO_DIRECTION_OUTPUT);
+    
     adc_init();
+    timer_init(TIMER0, timer);
+    OCR0 = 255;
+    
     adc_select_channel(0);
         
     while (1)
     {
       
         adc_start_conv();
+
         uint16_t res = adc_get_data();
         float volt =  (float)res * ADC_STEP;
+        
         if (volt < 0.75)
         {
             dio_set_level(LED_PORT, LED_PIN, 1);
@@ -31,7 +44,11 @@ void main()
         {
             dio_set_level(LED_PORT, LED_PIN, 0);
         }
-     
+        
+        if( dio_get_level(OPAMP10_PORT, OPAMP10_PIN) == DIO_LEVEL_HIGH) OCR0 = 0; //Duty Cycle is 100%
+		else if( dio_get_level(OPAMP20_PORT, OPAMP20_PIN) == DIO_LEVEL_HIGH) OCR0 = 102; //Duty Cycle is 60%
+		else if( dio_get_level(OPAMP30_PORT, OPAMP30_PIN) == DIO_LEVEL_HIGH) OCR0 = 178; //Duty Cycle is 30%
+
        
     }
 }
