ed0b9ae9d631e3d2a47cdf91b916f4231dca8afd
mohamed thabet <92416406+MAthabet@users.noreply.github.com>
2022-09-08 05:49:45 +0200

    Update timer.c

--- MCAL/TIMER/timer.c
+++ MCAL/TIMER/timer.c
@@ -1,27 +1,36 @@
+/*
+ * timer.c
+ *
+ * Created: 08/09/2022 04:58:54
+ *  Author: M-aa2
+ */ 
+
 #include "timer.h"
 #include <avr/io.h>
 
 
 void timer_init(timer_t timer, timer_config_t config)
 {
-    switch (timer)
-    {
-    case TIMER0:
-        // set_mode
-        TCCR0 = 0;
-        //set output mode
-        TCCR0 |= (config.output_mode % 2) << WGM00;
-        TCCR0 |= (config.output_mode / 2) << WGM01;
-        //clock source    
-        TCCR0 |= config.clksrc << CS00; 
-        //interrupt enable
-        if(config.output_mode == NORMAL)
-        {
-            TIMSK &= ~(0b1<<TOIE0);  
-        }
-        break;
+	switch (timer)
+	{
+		case TIMER0:
+		// set_mode
+		TCCR0 = 0;
+		//set output mode
+		TCCR0 |= (config.output_mode % 2) << WGM00;
+		TCCR0 |= (config.output_mode / 2) << WGM01;
+		
+		TCCR0 |= config.ctc_mode << COM00;
+		//clock source
+		TCCR0 |= config.clksrc << CS00;
+		//interrupt enable
+		if(config.output_mode == NORMAL)
+		{
+			TIMSK &= ~(0b1<<TOIE0);
+		}
+		break;
 
-    default:
-        break;
-    }
+		default:
+		break;
+	}
 }
